b3828e032bfda22c009c22088083855d70682455
Evgeniy Ivanov <p8635241@gmail.com>
2023-06-06 00:25:36 +0500

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,6 @@ type StoreType = {
 }
 
 function App() {
-    const [inputMin, setInputMin] = useState(0)
-    const [inputMax, setInputMax] = useState(5)
-
-    const [counter, setCounter] = useState(inputMin)
-    const [displayText, setDisplayText] = useState(true)
-    const [resetButtonState, setResetButtonState] = useState(false)
-    const [incButtonState, setIncButtonState] = useState(false)
-    const [settingsButtonState, setSettingsButtonState] = useState(false)
 
     const [store, setStore] = useState<StoreType>({
         inputMin: 0,
@@ -35,94 +27,62 @@ function App() {
         incButtonState: false,
         settingsButtonState: false
     })
-    const disableValue = 0
-    const inputMaxLowerThenInputMin = inputMax <= inputMin
-    const counterEqualToInputMax = counter === inputMax
-    const disableValueHigherThenInputMin = inputMin < disableValue
-    const counterEqualNull = counter === null
 
-    const disableValue2 = store.disableValue
-    const inputMaxLowerThenInputMin2 = store.inputMax <= store.inputMin
-    const counterEqualToInputMax2 = store.counter === store.inputMax
-    const disableValueHigherThenInputMin2 = store.inputMin < store.disableValue
-    const counterEqualNull2 = store.counter === null
+    const inputMaxLowerThenInputMin = store.inputMax <= store.inputMin
+    const counterEqualToInputMax = store.counter === store.inputMax
+    const disableValueHigherThenInputMin = store.inputMin < store.disableValue
+    const counterEqualNull = store.counter === null
 
     useEffect(() => {
         const minValue = localStorage.getItem("Minimum counter value")
         const maxValue = localStorage.getItem("Maximum counter value")
         if (minValue && maxValue) {
-            setInputMin(JSON.parse(minValue))
-            setInputMax(JSON.parse(maxValue))
-            setCounter(JSON.parse(minValue))
-            setStore({...store,
+            setStore({
+                ...store,
                 inputMin: JSON.parse(minValue),
                 inputMax: JSON.parse(maxValue),
-                counter: JSON.parse(minValue)})
+                counter: JSON.parse(minValue)
+            })
         }
     }, [])
 
 
     const setterHandler = () => {
-        setCounter(inputMin)
-        setDisplayText(true)
-        setResetButtonState(false)
-        setIncButtonState(false)
-        setSettingsButtonState(true)
-        localStorage.setItem("Minimum counter value", JSON.stringify(inputMin))
-        localStorage.setItem("Maximum counter value", JSON.stringify(inputMax))
-    }
-
-    const setterHandlerTwooo = () => {
-        setStore({...store,
-        displayText: true,
-        resetButtonState: false,
-        incButtonState: false,
-        settingsButtonState: true,
+        setStore({
+            ...store,
+            counter: store.inputMin,
+            displayText: true,
+            resetButtonState: false,
+            incButtonState: false,
+            settingsButtonState: true,
         })
-        localStorage.setItem("Minimum counter value", JSON.stringify(inputMin))
-        localStorage.setItem("Maximum counter value", JSON.stringify(inputMax))
+        localStorage.setItem("Minimum counter value", JSON.stringify(store.inputMin))
+        localStorage.setItem("Maximum counter value", JSON.stringify(store.inputMax))
     }
 
-    const counterHandler = () => setCounter(state => state + 1)
-    const counterHandlerTwooo = () => setStore({...store, counter: store.counter +1})
+
+    const counterHandler = () => setStore({...store, counter: store.counter + 1})
 
 
-    const resetCounter = () => setCounter(inputMin)
+    const resetCounter = () => setStore({...store, counter: store.inputMin})
 
-    const resetCounterTwo = () => setStore({...store, counter: store.inputMin})
 
     const inputMinValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setCounter(NaN)
-        setSettingsButtonState(false)
-        setResetButtonState(true)
-        setIncButtonState(true)
-        setDisplayText(false)
-        setInputMin(Number(e.currentTarget.value))
-
-    }
-
-    const inputMinValueHandlerTwooo = (e: ChangeEvent<HTMLInputElement>) => {
-        setStore({...store,
+        setStore({
+            ...store,
             counter: NaN,
-        settingsButtonState: false,
-        resetButtonState: true,
-        incButtonState: true,
+            settingsButtonState: false,
+            resetButtonState: true,
+            incButtonState: true,
             displayText: false,
             inputMin: Number(e.currentTarget.value)
         })
     }
 
-    const inputMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setCounter(NaN)
-        setSettingsButtonState(false)
-        setResetButtonState(true)
-        setIncButtonState(true)
-        setDisplayText(false)
-        setInputMax(Number(e.currentTarget.value))
-    }
 
-    const inputMaxValueHandlerTwooo = (e: ChangeEvent<HTMLInputElement>) => {
-        setStore({...store,
+    const inputMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setStore({
+            ...store,
             counter: NaN,
             settingsButtonState: false,
             resetButtonState: true,
@@ -140,27 +100,28 @@ function App() {
         <div className='main'>
 
             <div className="inputs">
-                <div className="input">Minimum value:<Input value={inputMin} type={"number"}
+                <div className="input">Minimum value:<Input value={store.inputMin} type={"number"}
                                                             onChange={inputMinValueHandler}
                                                             error={inputMaxLowerThenInputMin || disableValueHigherThenInputMin}/>
                 </div>
-                <div className="input">Maximum value:<Input value={inputMax} type={"number"}
+                <div className="input">Maximum value:<Input value={store.inputMax} type={"number"}
                                                             onChange={inputMaxValueHandler}
                                                             error={inputMaxLowerThenInputMin}/></div>
                 <div className="buttonSetter"><Button callback={setterHandler} name={"setter"}
-                                                      disabled={settingsButtonState || disableValueHigherThenInputMin || inputMaxLowerThenInputMin}/>
+                                                      disabled={store.settingsButtonState || disableValueHigherThenInputMin || inputMaxLowerThenInputMin}/>
                 </div>
             </div>
 
 
             <div className="inputs">
-                <CounterDisplay counterLimit={counterEqualToInputMax} counterValue={counter} displayText={displayText}
+                <CounterDisplay counterLimit={counterEqualToInputMax} counterValue={store.counter}
+                                displayText={store.displayText}
                                 errorLimit={inputMaxLowerThenInputMin || disableValueHigherThenInputMin}/>
                 <div className="buttonsDisplay"><span className="buttonSetter"> <Button callback={counterHandler}
                                                                                         name={"counter"}
-                                                                                        disabled={incButtonState || counterEqualToInputMax || counterEqualNull}/></span>
+                                                                                        disabled={store.incButtonState || counterEqualToInputMax || counterEqualNull}/></span>
                     <span className="buttonSetter"><Button callback={resetCounter} name={"reset"}
-                                                           disabled={resetButtonState || counterEqualNull}/></span>
+                                                           disabled={store.resetButtonState || counterEqualNull}/></span>
                 </div>
             </div>
         </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,7 @@ type DefaulInputPropsType = DetailedHTMLProps<
 type ButtonType = DefaulInputPropsType & {
     callback: ()=>void
 }
+
 export const Button= ({callback, name, disabled, ...restProps}:ButtonType) => {
     return (
         <button onClick={callback} disabled={disabled} {...restProps}>{name}</button>
