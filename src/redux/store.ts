9a8ad013ba94c77b82b2eecd8982135cd00ed9c5
Evgeniy Ivanov <p8635241@gmail.com>
2023-06-27 00:15:05 +0500

    lodash lib added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./counter-reducer.ts";
 import {loadState, saveState} from "../utils/localStorageUtils.ts";
+import {throttle} from "lodash";
+
 
 
 
@@ -13,11 +15,16 @@ const rootReducer = combineReducers({
 const store =legacy_createStore(rootReducer,loadState())
 console.log(store.getState().counter)
 
-store.subscribe(() => {
-    console.log(store.getState().counter)
-   saveState(store.getState().counter)
-})
+// store.subscribe(() => {
+//     console.log(store.getState().counter)
+//    saveState(store.getState().counter)
+// })
+
+store.subscribe(throttle(() => {
+    saveState( store.getState().counter);
+}, 1000));
 // export type StoreType = typeof store
 export type AppStateType = ReturnType<typeof rootReducer>
 
 export default store
+
