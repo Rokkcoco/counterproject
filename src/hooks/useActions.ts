f4ad2c61d81af5ea50554e790e828a11a6e9c5f3
Evgeniy Ivanov <p8635241@gmail.com>
2023-06-26 18:30:03 +0500

    code refactor

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,8 +1,8 @@
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
-import * as UserActionCreators from "../redux/action-creators.ts";
+import * as CounterActionCreators from "../redux/action-creators.ts";
 
 export const useActions = () => {
     const dispatch = useDispatch()
-    return bindActionCreators(UserActionCreators, dispatch)
+    return bindActionCreators(CounterActionCreators, dispatch)
 }
