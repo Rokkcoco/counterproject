77b6f40289a768cc60d6235e91412dfde02c6dc0
Evgeniy Ivanov <p8635241@gmail.com>
2023-07-22 13:50:06 +0500

    useActions updated with useMemo

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,8 +1,9 @@
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import * as CounterActionCreators from "../redux/action-creators.ts";
+import {useMemo} from "react";
 
 export const useActions = () => {
     const dispatch = useDispatch()
-    return bindActionCreators(CounterActionCreators, dispatch)
+    return useMemo(() => bindActionCreators(CounterActionCreators, dispatch),[])
 }
